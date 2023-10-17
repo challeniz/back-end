0a2849a2af579c2f1f7cd87a87b804688da3a5e5
[SW5] 이윤지 <dbswl342@gmail.com>
2023-10-17 15:55:52 +0900

    backend

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module} from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
@@ -9,7 +9,15 @@ import { ReviewModule } from './review/review.module';
 import { BadgesModule } from './badges/badges.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), MongooseModule.forRoot(process.env.MONGODB_URL), UsersModule, AuthModule, ChallengesModule, PostsModule, ReviewModule, BadgesModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(process.env.MONGODB_URL),
+    UsersModule,
+    AuthModule,
+    ChallengesModule,
+    PostsModule,
+    ReviewModule,
+    BadgesModule,
+  ],
 })
-
 export class AppModule {}
